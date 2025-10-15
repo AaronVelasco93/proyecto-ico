ce02ea5f03aa6cea9f8dcfbd44e20f0417c47c2b
AaronVelasco93 <aaronvelasco1993@outlook.com>
2025-10-15 10:50:37 -0600

    cambios por aaron

--- leer.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main(){ 
-    char nombre[10];
-    printf("Cual es tu nombre:")
-    gets(nombre);
-    printf("Su nombre es: %s",nombre);
-    
-
-}
---
+++ leer.js
@@ -0,0 +1,25 @@
+//              0      1       2         
+
+let frutas= ['fresa','pera','platano'];
+console.log(frutas);
+let gato ={
+    nombre: 'kikis',
+    edad: 12,
+    color: 'moteado'
+}
+console.log(gato);
+
+/*
+let numero1 =  parseInt( prompt("Ingresa el numero 1"));
+let numero2 = parseInt( prompt("Ingresa el numero 2"));
+console.log( typeof(numero1));
+
+let resultado = numero1+numero2;
+alert(resultado);
+
+
+
+var
+let 
+const
+*/
