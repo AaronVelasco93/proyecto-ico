34d90a3be1d73bec2370c046c992ec0936aebd7c
AaronVelasco93 <aaronvelasco1993@outlook.com>
2025-10-15 11:22:14 -0600

    cambios por : Emilio

---
+++ comparanumeros.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main(){ 
+int Num1=8;
+int Num2=33;
+
+if (Num1 == Num2){
+    printf("Son iguales");
+}
+if(Num1 > Num2){
+    printf("Numero 1 es mayor");
+}else{
+    printf("Numero 2 es mayor");
+}
+
+    
+
+    
+
+}
